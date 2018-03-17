91d829475e5621807ccf0e9fe7cbe37f80d80956
Martin <madetho@live.de>
2018-03-17 11:25:07 +0100

    append old messages

--- src/app/model/oldMessage.ts
+++ src/app/model/oldMessage.ts
@@ -1,9 +1,11 @@
 import { User } from "./user";
+import { Webm } from "./tenorGifs";
 
 export interface OldMessage {
     created: Date;
     _id: string;
     _creator: User;
     msg: string;
+    webm: Webm;
     __v: number;
 }
